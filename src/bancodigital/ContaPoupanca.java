29ade5c6f9988e1fbcb60f0a0353665a49a3fc78
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-30 18:54:24 -0300

    Cria ContaPoupanca que estende Conta

---
+++ src/bancodigital/ContaPoupanca.java
@@ -0,0 +1,11 @@
+package bancodigital;
+
+public class ContaPoupanca extends Conta {
+    public ContaPoupanca(int numero, int agencia) {
+        super(numero, agencia);
+    }
+
+    public ContaPoupanca(int numero, int agencia, double saldo) {
+        super(numero, agencia, saldo);
+    }
+}
