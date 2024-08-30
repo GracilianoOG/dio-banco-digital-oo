97318a12fc184f7e1b4e738c3d82708abfa4c0a1
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-30 20:40:09 -0300

    Teste com todas as operações disponíveis

--- src/bancodigital/Main.java
+++ src/bancodigital/Main.java
@@ -7,10 +7,39 @@ import bancodigital.conta.ContaPoupanca;
 public class Main {
     public static void main(String[] args) {
         Cliente bob = new Cliente("Bob");
+        Cliente john = new Cliente("John");
 
+        // Cria contas
         Conta cp = new ContaPoupanca(1111, 1, 1000, bob);
-        Conta cc = new ContaCorrente(2222, 2, 500, bob);
+        Conta cc = new ContaCorrente(2222, 2, 500, john);
 
+        // Imprime as contas iniciais
+        cp.imprimirExtrato();
+        cc.imprimirExtrato();
+
+        // Realiza depósito e saque
+        cp.depositar(1000);
+        cc.sacar(250);
+
+        // Imprime os extratos após as operações
+        cp.imprimirExtrato();
+        cc.imprimirExtrato();
+
+        // Não é possível sacar um valor maior que o da conta
+        cp.sacar(2500);
+        cc.sacar(300);
+
+        // Transferência
+        cp.transferir(1000, cc);
+
+        // Imprime os extratos após as transferências
+        cp.imprimirExtrato();
+        cc.imprimirExtrato();
+
+        // Não é possível transferir mais do que o saldo disponível
+        cp.transferir(5000, cc);
+
+        // Imprime os extratos após as transferências
         cp.imprimirExtrato();
         cc.imprimirExtrato();
     }
