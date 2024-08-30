77569cc0c99f50d2c5c805f08a834abdc5d4e419
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-30 18:54:05 -0300

    Cria ContaCorrente que estende Conta

---
+++ src/bancodigital/ContaCorrente.java
@@ -0,0 +1,11 @@
+package bancodigital;
+
+public class ContaCorrente extends Conta {
+    public ContaCorrente(int numero, int agencia) {
+        super(numero, agencia);
+    }
+
+    public ContaCorrente(int numero, int agencia, double saldo) {
+        super(numero, agencia, saldo);
+    }
+}
