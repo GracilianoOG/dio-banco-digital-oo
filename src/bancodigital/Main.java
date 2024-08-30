231db6e96860f8b38f35f2397ffb10a75e4198a0
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-30 18:58:24 -0300

    Instancia duas contas: corrente e poupança

--- src/bancodigital/Main.java
+++ src/bancodigital/Main.java
@@ -1,7 +1,15 @@
 package bancodigital;
 
+import bancodigital.conta.Conta;
+import bancodigital.conta.ContaCorrente;
+import bancodigital.conta.ContaPoupanca;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Conta cp = new ContaPoupanca(1111, 1, 1000);
+        Conta cc = new ContaCorrente(2222, 2, 500);
+
+        cp.imprimirInformacoesConta();
+        cc.imprimirInformacoesConta();
     }
 }
